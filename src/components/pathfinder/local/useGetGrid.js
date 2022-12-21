0902bfeaabace835cfd250b90b2216ce995f8103
John Lloyd <johnloyd.centeno@gmail.com>
2022-12-21 19:51:00 +0800

    New Cell Size

--- src/components/pathfinder/local/useGetGrid.js
+++ src/components/pathfinder/local/useGetGrid.js
@@ -8,14 +8,7 @@ const normalizedValue = (value, size) => {
 const getDensity = (width, height, size) => {
   const row = normalizedValue(height, size);
   const col = normalizedValue(width, size);
-
-  const widthDiff = width / col,
-    heightDiff = height / row;
-
-  let increaseX = heightDiff - widthDiff >= 1.15 ? 2 : 0;
-  let increaseY = widthDiff - heightDiff >= 1.15 ? 2 : 0;
-
-  return { count: col * row, col: col + increaseX, row: row + increaseY };
+  return { count: col * row, col: col, row: row };
 };
 
 const getDimension = ({ scaleX, scaleY }) => {
